f218127d80bdb0a5c5839ef8fabd43ef1ebcd424
Albert Aleksa <albert.aleksa.by@gmail.com>
2020-06-08 12:29:35 -0500

    add param verbose_name for fields in class Bb for better view in admin site

--- bboard/models.py
+++ bboard/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Product')
+    content = models.TextField(null=True, blank=True, verbose_name='Description')
+    price = models.FloatField(null=True, blank=True, verbose_name='Price')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Published date')
+
+    class Meta:
+        verbose_name_plural = 'Advertisements'
+        verbose_name = 'Advertisement'
+        ordering = ['-published']
--- bboard/views.py
+++ bboard/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import Bb
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     context = {'bbs': bbs}
     return render(request, 'bboard/index.html', {'bbs': bbs})

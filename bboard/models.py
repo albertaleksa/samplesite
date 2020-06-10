589a40c33b6fe5f524a957da4e7092d4d9d9a6c8
Albert Aleksa <albert.aleksa.by@gmail.com>
2020-06-10 16:29:51 -0500

    url adresses

--- bboard/models.py
+++ bboard/models.py
@@ -5,8 +5,20 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Description')
     price = models.FloatField(null=True, blank=True, verbose_name='Price')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Published date')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric')
 
     class Meta:
         verbose_name_plural = 'Advertisements'
         verbose_name = 'Advertisement'
         ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Rubrics'
+        verbose_name = 'Rubric'
+        ordering = ['name']
